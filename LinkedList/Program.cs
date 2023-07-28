d5ca07f56808e9ec81cb9be6bebf0b0b242c5fc9
Narenthra Kishore RS <narenthrakishore.rs@tvsnext.com>
2023-07-28 12:17:18 +0530

    [Narenthra Kishore] Modify : According to the Use case

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -6,28 +6,24 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
-
             LinkedList list = new LinkedList();
-            list.Add(1);
-            list.Add(2);
-            list.Add(3);
-            list.Add(4);
-            list.Add(5);
-            list.Add(6);
-            list.Add(7);
-            list.Display();
-            list.DeleteNodeAtParticularPosition(3);
-            list.Display();
-            list.Search(7);
-            list.Display();
-            list.RemoveLastNode();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
             list.Display();
             list.RemoveFirstNode();
             list.Display();
-            list.InsertAtParticularPosition(3,3);
-            list.Display();
-            list.AddInReverseOrder(0);
-            list.Display();
+            list.RemoveLastNode();
+            list.Search(30);
+            list.Add(40);
+
+            Stack stack = new Stack();
+            stack.push(56);
+            stack.push(30);
+            stack.push(70);
+            stack.peek();
+
+
         }
     }
 }
---
+++ LinkedList/Stack.cs
@@ -0,0 +1,72 @@
+﻿using LinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+
+{
+    internal class Stack
+    {
+        Node top;
+        public Stack()
+        {
+            this.top = null;
+        }
+        internal void push(int data)
+        {
+            Node node = new Node(data);
+            if (this.top == null)
+            {
+                node.next = null;
+            }
+            else
+            {
+                node.next = this.top;
+            }
+            this.top = node;
+            Console.WriteLine("{0} is pushed to stack", data);
+        }
+        internal void Display()
+        {
+            Node temp = this.top;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " | ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
+        internal void peek()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("Stack is empty");
+                return;
+            }
+            Console.WriteLine("{0} is in the top", this.top.data);
+        }
+        internal void pop()
+        {
+            if (this.top == null)
+            {
+                Console.WriteLine("stack is empty");
+            }
+            else
+            {
+                Console.WriteLine("the value poped is {0}", this.top.data);
+                this.top = this.top.next;
+            }
+        }
+        internal void IsEmpty()
+        {
+            while (this.top != null)
+            {
+                peek();
+                pop();
+            }
+        }
+    }
+}
